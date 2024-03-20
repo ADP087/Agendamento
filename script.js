f4f85e416c5ef038d8b0ffaf4c7daae90753fc4f
Vinicius Alves <139510555+Vitenskapp@users.noreply.github.com>
2024-03-20 23:37:28 +0000

    Tratamento do menu mobile

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const menuIcon = document.querySelector('#menuIcon');
 const menu = document.querySelector('.menu');
 
-menuIcon.addEventListener('click', () => {
-  if(menu.style.display === "flex"){
-    menu.setAttribute('style', 'display: none; animation: hideMenu 0.75s ease-in-out;');
-    menuIcon.innerHTML = "menu"
+menuIcon.addEventListener('click', () => { if(menu.style.display === "flex"){
+    menu.setAttribute('style', 'display: none; animation: hideMenu 0.75s ease-in-out !important;')
+    menuIcon.innerHTML = "menu";
+  document.body.style.overflow = "auto";
   } else {
-    menu.setAttribute('style', 'display: flex; animaion: showMenu 0.75s ease-in-out;');
-    menuIcon.innerHTML = "close"
+    menu.setAttribute('style', 'display: flex;')
+    menuIcon.innerHTML = "close";
+    document.body.style.overflow = "hidden";
   }
 })
