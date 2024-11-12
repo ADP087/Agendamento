4556e1ccd0d073508e92edf4212d1eb6aec143ed
ADP <arthur.passarelli087@gmail.com>
2024-11-11 22:43:09 -0300

    Fazendo as "Informações"
    Criei as paginas de "Informações", tem coisas pra mexer ainda e fazer a página "nosso time"

--- Modal/login.php
+++ Modal/login.php
@@ -65,6 +65,7 @@ if (isset($_SESSION['error_message'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../assets/icon.png">
     <link rel="stylesheet" href="estilo.css">
     <title>Login</title>
 
